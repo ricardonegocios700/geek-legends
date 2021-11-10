a159ed3c7fea8fabc84363fd273cea2ce3e17d17
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-10 18:28:11 -0300

    Feat: added Auth and XFile providers

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -1,12 +1,20 @@
 import { ReactNode } from "react";
 import { MultimediaProvider } from "./multimedia/";
+import { AuthProvider } from "./user";
+import { XFileProvider } from "./xfile";
 
 interface ProvidersProps {
     children: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-    return <MultimediaProvider>{children}</MultimediaProvider>;
+    return (
+        <AuthProvider>
+            <MultimediaProvider>
+                <XFileProvider>{children}</XFileProvider>
+            </MultimediaProvider>
+        </AuthProvider>
+    );
 };
 
 export default Providers;
