564c18eeae1327b73a174935ed220326bf3ad474
BeatrixDev <beatrix.ford@hotmail.com>
2021-11-10 20:31:51 -0300

    feat: insert GroupProvider inside index providers

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -2,19 +2,22 @@ import { ReactNode } from "react";
 import { MultimediaProvider } from "./multimedia/";
 import { AuthProvider } from "./user";
 import { XFileProvider } from "./xfile";
+import { GroupProvider } from "./group";
 
 interface ProvidersProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const Providers = ({ children }: ProvidersProps) => {
-    return (
-        <AuthProvider>
-            <MultimediaProvider>
-                <XFileProvider>{children}</XFileProvider>
-            </MultimediaProvider>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <MultimediaProvider>
+        <XFileProvider>
+          <GroupProvider>{children}</GroupProvider>
+        </XFileProvider>
+      </MultimediaProvider>
+    </AuthProvider>
+  );
 };
 
 export default Providers;
