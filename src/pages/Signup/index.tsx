26a6fb56c8568ba7a6d918399d9a0bf78010e88e
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-11 19:00:09 -0300

    Feat: signup page

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,8 +1,14 @@
+import { Container } from "./styles";
+
 const Signup = () => {
 
 
     return(
-        <></>
+        <>
+        <Container>
+
+        </Container>        
+        </>
     )
 }
 
--- src/pages/Signup/styles.ts
+++ src/pages/Signup/styles.ts
@@ -1,1 +1,8 @@
-export {}
+import styled from 'styled-components';
+import SignupBG from '../../assets/signupBG-desktop.jpg';
+
+
+export const Container = styled.div`
+    background: url(${SignupBG});
+
+`;
