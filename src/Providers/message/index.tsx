8cae17debb9140b0fbf8c32227d11b2ec38beee1
Ricardo <ricardonegocios700@gmail.com>
2021-11-11 15:42:41 -0300

    feat(messagesProvider): created initial context

--- src/Providers/message/index.tsx
+++ src/Providers/message/index.tsx
@@ -1,1 +1,16 @@
-export {}
+import { createContext, useContext } from "react";
+
+interface MessagesProviderDate {
+  id?: number;
+  quote: string;
+  translate: string;
+  author: string;
+}
+
+const MessagesContext = createContext<MessagesProviderDate>(
+  {} as MessagesProviderDate
+);
+
+export const useMessages = () => {
+  useContext(MessagesContext);
+};
