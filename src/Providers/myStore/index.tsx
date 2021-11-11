d332431dee67554c09c2ec6a67fa410896ef0a59
Ricardo <ricardonegocios700@gmail.com>
2021-11-11 12:28:57 -0300

    feat(myStoresProvider): created base provider

--- src/Providers/myStore/index.tsx
+++ src/Providers/myStore/index.tsx
@@ -1,5 +1,7 @@
-import { createContext, useContext } from "react";
-
+import { createContext, ReactNode, useContext } from "react";
+interface MyStoresProviderProps {
+  children: ReactNode;
+}
 interface ContextType {
   name: string;
   url: string;
@@ -9,6 +11,15 @@ interface ContextType {
   id?: number;
 }
 const MyStoresContext = createContext<ContextType>({} as ContextType);
+
+export const MyStoresProvider = ({ children }: MyStoresProviderProps) => {
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpY2FyZG9AZW1haWwuY29tIiwiaWF0IjoxNjM2NjQ0MjY4LCJleHAiOjE2MzY2NDc4NjgsInN1YiI6IjQifQ.pikUOwuiB19zj5ayacJ4AQDEiv00RoRGOKIvAESYTgA";
+
+  return (
+    <MyStoresContext.Provider value={{}}>{children}</MyStoresContext.Provider>
+  );
+};
 
 export const useMyStores = () => {
   useContext(MyStoresContext);
