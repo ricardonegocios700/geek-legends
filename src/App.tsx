3c42b01380510af1471ee846028c7bf0c25374af
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-11 17:40:11 -0300

    Fix: added  toastify refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import Routes from "./Routes/routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <div>
-
-      <Routes />
-      APP
-    </div>
-  );
+    return (
+        <>
+        
+            <ToastContainer
+                theme="colored"
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+            <Routes />
+            
+        </>
+    );
 }
 
 export default App;
