f261c8c5a0fe76b58b59f5f61b26fa45a6e951d9
Ricardo <ricardonegocios700@gmail.com>
2021-11-11 14:10:14 -0300

    feat(contextStores): import of useAuth

--- src/Providers/myStore/index.tsx
+++ src/Providers/myStore/index.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 import api from "../../services/api";
+import { useAuth } from "../user";
 
 interface ProviderProps {
   children: ReactNode;
@@ -21,6 +22,9 @@ const MyStoresContext = createContext<MyStoresProviderDate>(
 );
 
 export const MyStoresProvider = ({ children }: ProviderProps) => {
+  {
+    /*const { config, accessToken } = useAuth();*/
+  }
   const accessToken =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpY2FyZG9AZW1haWwuY29tIiwiaWF0IjoxNjM2NjUwMTAwLCJleHAiOjE2MzY2NTM3MDAsInN1YiI6IjQifQ.5W2Ief_S0afUu40WLeFVa8GgBPe3Yb0JpoussWRjO1Q";
   const config = { headers: { Authorization: `Bearer ${accessToken}` } };
