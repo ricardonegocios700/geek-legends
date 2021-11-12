eb2ba5d17bb3058536d250b5e0f647878cf3972a
BeatrixDev <beatrix.ford@hotmail.com>
2021-11-12 09:30:04 -0300

    feat: add code basic inside dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,19 @@
+import InfoGeek from "../../components/infoGeek";
+import { Container } from "./styles";
+
 const Dashboard = () => {
-  return <></>;
+  return (
+    <Container>
+      <header>componente de cima</header>
+      <div>
+        <InfoGeek />
+        <section>
+          <div>geek treasure</div>
+          <div>Message sansei</div>
+        </section>
+      </div>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,1 +1,14 @@
-export {}
+import styled from "styled-components";
+//import backCity from "../../assets/dashboardBG-desktop.gif"; to do
+
+export const Container = styled.main`
+  height: 95vh;
+  background-image: url("https://i.gifer.com/xK.gif");
+  background-size: cover;
+  background-repeat: no-repeat;
+  header {
+    background: lightblue;
+    width: 100vw;
+    height: 15vh;
+  }
+`;
