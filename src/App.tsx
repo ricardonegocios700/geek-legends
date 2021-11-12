a2a0de1b42a98900de32a844775ec3c7d4984b0f
Sthefany Gomes <fany@ubuntu.local>
2021-11-11 23:44:29 -0300

    feat: add globalStyle and variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import Routes from "./Routes/routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import GlobalStyle from "./styles/global";
 
 function App() {
-    return (
-        <>
-        
-            <ToastContainer
-                theme="colored"
-                position="top-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-            />
-            <Routes />
-            
-        </>
-    );
+  return (
+    <>
+      <GlobalStyle />
+      <ToastContainer
+        theme="colored"
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Routes />
+    </>
+  );
 }
 
 export default App;
