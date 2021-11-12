02f48b2292e33b1a5686678c05719725c26f10df
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-12 10:26:01 -0300

    Feat: main button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const ActionButton = styled.button`
+    width: 200px;
+    padding: 10px 30px;
+    border: transparent;
+    border-radius: 5px;
+    background-color: #e23e3e;    
+    padding: 15px 45px;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+    transition: 0.5s;    
+    box-shadow: 0 0 15px 0px #868b8e;
+    display: block;
+
+    :hover {        
+        filter: brightness(0.9);       
+    }
+`;
