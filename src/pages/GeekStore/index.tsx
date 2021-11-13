366f690b6911b1d6dfd7acfa897805b94ad552ce
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-13 12:48:45 -0300

    Feat: geek store

--- src/pages/GeekStore/index.tsx
+++ src/pages/GeekStore/index.tsx
@@ -1,16 +1,25 @@
 import Header from "../../components/Header";
-import { useLocation } from 'react-router-dom'
-
-
+import { Container } from "./styles";
+import Card from "../../components/Card";
 
 const GeekStore = () => {
-    
-
-    return(
+    return (
         <>
-        <Header />
+            <Header />
+            <div></div>
+            <Container>
+
+                <Card 
+                    img='https://idris.com.br/dev/wp-content/themes/Idris/timthumb.php?src=https://idris.com.br/dev/wp-content/uploads/2018/03/fantastic-full-metal-alchemist-poster-and-awesome-ideas-of-fullmetal-alchemist-brotherhood-768x1095.jpg&w=821&h=400&zc=1'
+                    dislike='0'
+                    like='3'
+                    handleAddPersona={() => null}
+                    handleClick={() => 1}
+                    name='nome'
+                />
+            </Container>
         </>
-    )
-}
+    );
+};
 
 export default GeekStore;
--- src/pages/GeekStore/styles.ts
+++ src/pages/GeekStore/styles.ts
@@ -1,1 +1,20 @@
-export {}
+import styled from "styled-components";
+import DesktopBack from "../../assets/myGeekStoreBG-desktop.gif";
+import MobileBack from "../../assets/myGeekStore-mobile.jpg";
+
+export const Container = styled.section`
+    width: 100vw;
+    height: 100vh;
+
+    background: url(${MobileBack});
+    background-size: cover;
+    background-position: center center;
+
+    @media only screen and (min-width: 400px) {
+    }
+    @media only screen and (min-width: 800px) {
+        background: url(${DesktopBack}) no-repeat;
+        background-size: cover;
+        background-position: center center;
+    }
+`;
