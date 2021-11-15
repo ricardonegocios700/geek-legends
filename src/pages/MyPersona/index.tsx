14e4a1c320e2a4cbcacc1a2c6917d98338821b56
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-15 13:53:16 -0300

    Feat: page animations

--- src/pages/MyPersona/index.tsx
+++ src/pages/MyPersona/index.tsx
@@ -1,13 +1,24 @@
 import Header from "../../components/Header";
+import { motion } from "framer-motion";
+
+const pageTransition = {
+    // ease: "easeIn",
+    duration: 1,
+};
 
 const MyPersona = () => {
-
-
-    return(
+    return (
         <>
-        <Header />
+            <motion.div
+                initial={{ opacity: 0, x: "-100vw" }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: "-100w" }}
+                transition={pageTransition}
+            >
+                <Header />
+            </motion.div>
         </>
-    )
-}
+    );
+};
 
 export default MyPersona;
