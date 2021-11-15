ad621bfb1c1fa1c5040ea4e396936c873b1bf608
Ricardo <ricardonegocios700@gmail.com>
2021-11-15 11:44:55 -0300

    feat(multimediaPage) created style file and put background

--- src/pages/Multimedia/index.tsx
+++ src/pages/Multimedia/index.tsx
@@ -1,16 +1,18 @@
 import Header from "../../components/Header";
 import TypesMultimedia from "../../components/TypesMultimedias";
-
+import { Container, Div } from "./styles";
 const Multimedia = () => {
   return (
-    <>
-      {/* <Header /> */}
-      <TypesMultimedia typeValue="Filmes" />
-      <TypesMultimedia typeValue="Animes" />
-      <TypesMultimedia typeValue="Gibis" />
-      <TypesMultimedia typeValue="Séries" />
-      <TypesMultimedia typeValue="Games" />
-    </>
+    <Container>
+      <Header />
+      <Div>
+        <TypesMultimedia typeValue="Filmes" />
+        <TypesMultimedia typeValue="Animes" />
+        <TypesMultimedia typeValue="Gibis" />
+        <TypesMultimedia typeValue="Séries" />
+        <TypesMultimedia typeValue="Games" />
+      </Div>
+    </Container>
   );
 };
 
--- src/pages/Multimedia/styles.ts
+++ src/pages/Multimedia/styles.ts
@@ -1,1 +1,28 @@
-export {}
+import styled from "styled-components";
+import BackImage from "../../assets/multimidia-desktop.gif";
+
+export const Div = styled.div`
+  margin-top: 130px;
+  display: flex;
+  flex-direction: row;
+  @media only screen and (min-width: 400px) {
+  }
+  @media only screen and (min-width: 800px) {
+  }
+`;
+
+export const Container = styled.section`
+  color: #fff;
+  background-color: var(--back-color);
+  @media only screen and (min-width: 400px) {
+  }
+  @media only screen and (min-width: 800px) {
+    background: url(${BackImage}) no-repeat;
+    background-size: cover;
+    background-position: center;
+    flex-direction: row;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+  }
+`;
