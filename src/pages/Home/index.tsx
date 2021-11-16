f94f692b6b077fda5df77dc8bae4eae60fd06fe1
Sthefany Gomes <fany@ubuntu.local>
2021-11-16 09:29:23 -0300

    feat: home page responsive layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,63 @@
-export {}
+import Button from "../../components/Button";
+import { ButtonsDiv, HomeContainer, TeamSection, TitleSection } from "./styles";
+import Beatriz from "../../assets/beatriz.png";
+import Roberto from "../../assets/roberto.png";
+import Ricardo from "../../assets/ricardo.png";
+import Sthefany from "../../assets/sthefany.png";
+import spider from "../../assets/spider-man.gif";
+import iron from "../../assets/iron-man.gif";
+import flash from "../../assets/flash.gif";
+import darth from "../../assets/darth_vader.gif";
 
 const Home = () => {
+  return (
+    <HomeContainer>
+      <TitleSection>
+        <div>
+          <img src={spider} alt="" />
+          <img src={iron} alt="" />
+        </div>
 
+        <h1>GeekLegends</h1>
 
-    return(
-        <></>
-    )
-}
+        <div>
+          <img src={flash} alt="" />
+          <img src={darth} alt="" />
+        </div>
+      </TitleSection>
+
+      <ButtonsDiv>
+        <Button type="submit" title="LOGIN" />
+        <Button type="submit" title="CADASTRO" />
+      </ButtonsDiv>
+
+      <TeamSection>
+        <h3>GEEK TEAM</h3>
+        <div className="containerDevs">
+          <div>
+            <img src={Beatriz} alt="" />
+            <h4>Beatriz</h4>
+            <h5> Scrum Master </h5>
+          </div>
+          <div>
+            <img src={Ricardo} alt="" />
+            <h4>Ricardo</h4>
+            <h5> Product Owner </h5>
+          </div>
+          <div>
+            <img src={Roberto} alt="" />
+            <h4>Roberto</h4>
+            <h5> Tech Leader </h5>
+          </div>
+          <div>
+            <img src={Sthefany} alt="" />
+            <h4>Sthéfany</h4>
+            <h5> Quality Assurance</h5>
+          </div>
+        </div>
+      </TeamSection>
+    </HomeContainer>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,163 @@
-export {}
+import homeBGdesktop from "../../assets/homeBG-desktop.png";
+import homeBGmobile from "../../assets/homeBG-mobile.jpg";
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+  background-image: url(${homeBGmobile});
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: var(--font-color-white);
+  font-family: var(--font-family-primary);
+
+  @media (min-width: 768px) {
+  }
+
+  @media (min-width: 1000px) {
+    background-image: url(${homeBGdesktop});
+    background-position: center;
+    background-size: cover;
+  }
+`;
+
+export const TitleSection = styled.section`
+  position: absolute;
+  bottom: 0;
+  font-size: 2rem;
+  width: 100%;
+  text-align: center;
+  margin: 20px;
+
+  img {
+    display: none;
+  }
+
+  div {
+    display: flex;
+  }
+
+  @media (min-width: 768px) {
+    width: 350px;
+    height: max-content;
+    top: 0;
+    left: 0;
+    margin: 40px;
+
+    h1 {
+      margin: 10px;
+    }
+    img {
+      display: flex;
+      width: 120px;
+      border-radius: 50%;
+    }
+  }
+
+  @media (min-width: 700px) {
+    div {
+      justify-content: space-between;
+    }
+  }
+
+  @media (min-width: 1000px) {
+    margin: 100px;
+    width: 480px;
+
+    h1 {
+      font-size: 3.5rem;
+    }
+
+    img {
+      width: 160px;
+    }
+  }
+`;
+
+export const ButtonsDiv = styled.div`
+  position: absolute;
+  top: 20vh;
+  display: flex;
+  flex-direction: column;
+  z-index: 1;
+
+  button {
+    margin: 15px;
+  }
+
+  @media (min-width: 546px) {
+    top: 30vh;
+  }
+
+  @media (min-width: 768px) {
+    top: 50%;
+  }
+
+  @media (min-width: 1000px) {
+    left: 10%;
+    top: 65%;
+
+    height: max-content;
+
+    button {
+      width: 300px;
+      margin: 25px;
+    }
+  }
+`;
+
+export const TeamSection = styled.section`
+  background-color: var(--blue-div);
+  display: flex;
+  flex-direction: column;
+  width: 75vw;
+  align-items: center;
+  text-align: center;
+  position: absolute;
+  bottom: 90px;
+  border-radius: 0 50px 0 50px;
+  padding: 10px;
+
+  .containerDevs {
+    max-height: 300px;
+    display: flex;
+    flex-flow: wrap;
+    justify-content: space-evenly;
+    padding: 15px;
+  }
+
+  .containerDevs > div {
+    margin: 3px;
+  }
+
+  .containerDevs > div > img {
+    width: 60px;
+  }
+
+  @media (min-width: 768px) {
+    width: 450px;
+    bottom: 0;
+    right: 0;
+
+    .containerDevs > div > img {
+      width: 70px;
+    }
+  }
+
+  @media (min-width: 1000px) {
+    /* position: relative; */
+    width: 40vw;
+    justify-content: space-between;
+
+    .containerDevs {
+      align-self: stretch;
+    }
+
+    .containerDevs > div > img {
+      width: 80px;
+    }
+  }
+`;
