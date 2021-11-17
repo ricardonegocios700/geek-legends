623f77940c74bc8ee717694fe8676ce11d9c0784
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-17 15:41:44 -0300

    Feat: added myPages links info

--- src/components/boxTreasure/styles.ts
+++ src/components/boxTreasure/styles.ts
@@ -2,40 +2,48 @@ import styled from "styled-components";
 import itensBox from "./../../assets/treasurechest.gif";
 
 export const ContainerTreasure = styled.div`
-  background-image: url(${itensBox});
-  background-size: cover;
-  width: 240px;
-  height: 240px;
-  text-align: center;
-  color: var(--font-color-white);
-  font-size: 30px;
-  display: flex;
-  flex-direction: column;
-  h4 {
-    height: 50px;
-  }
-  nav {
+    background-image: url(${itensBox});
+    background-size: cover;
+    width: 240px;
+    height: 240px;
+    text-align: center;
+    color: var(--font-color-white);
+    font-size: 30px;
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    padding: 20px;
-    a {
-      width: 22%;
-      height: 100%;
-
-      img {
-        width: 100%;
-        height: 100%;
-        border: 2px solid transparent;
-        border-radius: 50%;
-        &:hover {
-          border: gray;
-        }
-      }
+    flex-direction: column;
+    h4 {
+        height: 50px;
     }
-  }
-  @media (min-width: 800px) {
-    width: 200px;
-    height: 200px;
-  }
+    nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        padding: 10px;
+        a {
+            width: 22%;
+            height: 100%;
+
+            img {
+                width: 100%;
+                height: 100%;
+                border: 2px solid transparent;
+                border-radius: 50%;
+                &:hover {
+                    border: gray;
+                }
+            }
+        }
+    }
+    & p {
+        width: 80%;
+        font-size: 18px;
+        margin: 0 auto 10px;
+        font-family: var(--font-family-primary);
+        font-weight: 700;
+    }
+
+    @media (min-width: 800px) {
+        width: 200px;
+        height: 200px;
+    }
 `;
