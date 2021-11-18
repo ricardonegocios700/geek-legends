b9b84f7e6096d94818763989ec936e240c8a519e
Roberto Bressanelli <rbressanelli@gmail.com>
2021-11-18 12:59:08 -0300

    Fix: background img and header scroll

--- src/pages/MyGeekStore/styles.ts
+++ src/pages/MyGeekStore/styles.ts
@@ -4,9 +4,9 @@ import MobileBack from "../../assets/myGeekStore-mobile.jpg";
 
 export const Container = styled.section`
     width: 100%;
-    height:100%;
+    height: 100%;
     min-height: 100vh;
-    padding: 150px 70px 0;
+    padding: 150px 30px 0;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
@@ -14,12 +14,19 @@ export const Container = styled.section`
     background: url(${MobileBack});
     background-size: fill;
     background-position: center center;
+    background-size: cover;
+    background-position: fixed;
+    background-attachment: fixed;
+    
+
 
     @media only screen and (min-width: 400px) {
     }
     @media only screen and (min-width: 800px) {
         background: url(${DesktopBack}) no-repeat;
-        background-size: cover;
-        background-position: center center;        
+        background-size: cover;        
+        background-attachment: fixed;
+
+        padding: 150px 70px 0;
     }
 `;
